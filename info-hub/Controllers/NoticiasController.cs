f24514fc3eca37bb768c999344330f583f26b50d
Luana Scardua <luanascardua7@gmail.com>
2024-05-05 02:08:37 -0300

    feat: add aplicativo web ASP.NET Core (MVC)

---
+++ info-hub/Controllers/NoticiasController.cs
@@ -0,0 +1,15 @@
+﻿using info_hub.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace info_hub.Controllers
+{
+    public class NoticiasController(AppDbContext context) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var news_data = await context.Noticias.ToListAsync();
+            return View(news_data);
+        }
+    }
+}
---
+++ info-hub/Models/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace info_hub.Models
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Noticia> Noticias { get; set; }
+    }
+}
