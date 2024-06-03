3653a41bf3ffc7d8578a99d54037f742240c800c
Nicholas Deway <nicholasdeway@hotmail.com>
2024-06-03 00:11:28 -0300

    feat: adicionada atualizações relacionada ao layout e responsividade

--- info-hub/Controllers/HomeController.cs
+++ info-hub/Controllers/HomeController.cs
@@ -18,10 +18,6 @@ namespace info_hub.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
