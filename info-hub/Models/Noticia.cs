393ce09a5c861ee69bf807e1baea45cd3e48f406
Nicholas Deway <nicholasdeway@hotmail.com>
2024-06-09 22:42:01 -0300

    feat: CRUD Projeto

---
+++ info-hub/Models/Noticia.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace info_hub.Models
+{
+    [Table("Noticias")]
+    public class Noticia
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Error! Obrigatório informar o tema")]
+        public required string Tema { get; set; }
+
+        [Required(ErrorMessage = "Error! Obrigatório informar o título")]
+        public required string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Error! Obrigatório informar a data de publicação")]
+        public required string DataPublicacao { get; set; }
+
+        public ICollection<Tema> Temas { get; set; }
+    }
+}
