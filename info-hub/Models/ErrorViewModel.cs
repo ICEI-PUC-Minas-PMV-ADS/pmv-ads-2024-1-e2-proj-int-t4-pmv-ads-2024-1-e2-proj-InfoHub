0c1c9f9f55c8b4a35b176f5f9535338f8ae74138
Luana Scardua <92794401+luanascardua@users.noreply.github.com>
2024-06-09 22:06:22 -0300

    Revert "Feature/front end"

--- info-hub/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace info_hub.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
