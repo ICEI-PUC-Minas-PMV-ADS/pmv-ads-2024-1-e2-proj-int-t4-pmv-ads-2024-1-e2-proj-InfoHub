83fe15cfdd313ed7fa8221b5b61c7cb915edb471
Guilherme Morato <morato12gui@gmail.com>
2024-06-23 21:29:53 -0300

    feat: add controllers categorias

--- info-hub/Controllers/HomeController.cs
+++ info-hub/Controllers/HomeController.cs
@@ -17,6 +17,10 @@ namespace info_hub.Controllers
         {
             return View();
         }
+        public IActionResult Politica()
+        {
+            return View();
+        }
 
         // Nova ação para a página "Mundo"
         public IActionResult Mundo()
